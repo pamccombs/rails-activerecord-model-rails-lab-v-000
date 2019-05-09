ed9eaf2df64430461e23a631561b25e1026dbf61
Patrick McCombs <pa.mccombs1@gmail.com>
2019-05-09 23:20:29 +0000

    Automatically backed up by Learn

--- db/migrate/20190509231800_students.rb
+++ db/migrate/20190509231800_students.rb
@@ -1,7 +1,7 @@
 class Students < ActiveRecord::Migration[5.0]
   def change
     create_table :students do |t|
-      t.string :name
+      t.string :first_name
       t.text :description
  
       t.timestamps
